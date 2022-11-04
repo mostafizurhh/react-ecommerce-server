5bade7fe3a2e345a07b8ee86247d97d48d8e1910
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-04 16:14:32 +0000

    (READ) created API to get/read all data from DB

--- index.js
+++ index.js
@@ -23,3 +23,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        /* created a DB in server and mongoDB */
+        const productCollection = client.db('react-ecommerce').collection('products');
+
+        /*(READ) created API to get/read all data from DB*/
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
